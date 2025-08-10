b8e8a215581f4ba83b31530baaa5c7255b881dc9
AbduDark <abdu.ewian@gmail.com>
2025-08-10 14:24:11 +0300

    fixed

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,7 @@ Route::prefix('auth')->middleware('throttle:10,1')->group(function () {
 // Guest-accessible courses
 Route::get('courses',       [CourseController::class, 'index']);
 Route::get('courses/{id}',  [CourseController::class, 'show']);
-Route::get('courses/{id}/ratings', [RatingController::class, 'index']);</old_str>
+Route::get('courses/{id}/ratings', [RatingController::class, 'index']);
 
 // Authenticated User Routes
 Route::middleware(['auth:sanctum'])->group(function () {
